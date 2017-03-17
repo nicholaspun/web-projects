14d9f421de7dac62c7e6066c0273d92955140a7d
Nicholas Pun <nicholaspun99@gmail.com>
2017-03-17 15:55:00 -0400

    TIC-TAC-TOE: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tic Tac Toe
+
+Another small project, made while following Facebook's React Tutorial:
+https://facebook.github.io/react/tutorial/tutorial.html
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Game from 'components/game';
+
+render(<Game />, document.getElementById('game'));
