4f7cf73027145df68238298919c3d69a1eb051ba
Nicholas Pun <nicholaspun99@gmail.com>
2017-04-04 00:28:19 -0400

    PHOTO-GALLERY: Fixed footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import img from "../data/GitHub-Mark-Light-64px.png";
 
 const Footer = () => {
-  <a href="https://github.com/nicholaspun">
-    <img src={img} alt="github logo" className="logo" />
-  </a>
+  return (
+    <a href="https://github.com/nicholaspun">
+      <img src={img} alt="github logo" className="logo" />
+    </a>
+  )
 }
 
 export default Footer;
