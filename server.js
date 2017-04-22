4db6bb9a6aefd4bfd856a721990650087eff66b1
Nicholas Pun <nicholaspun99@gmail.com>
2017-04-22 19:16:05 -0400

    SOCKET-IO-CHAT-APP: first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log("Server running ... ");
+
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Open up connection with Socket.IO
+io.sockets.on('connection', function(socket) {
+  // All the events we will emit will go here
+  connections.push(socket);
+  console.log("Connected: %s sockets connected", connections.length);
+
+  // Disconnect
+  socket.on('disconnect', function(data) {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("Disconnected: %s sockets connected", connections.length);
+  }) 
+})
